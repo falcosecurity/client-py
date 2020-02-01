0647cd1b701eaf1a8cda8c247c19f05c06a1289f
Mattia <mattia@airmail.cc>
2020-02-01 21:17:06 +0100

    Add grpc channel credentials
    Signed-off-by: Mattia <mattia@airmail.cc>

---
+++ falco/__init__.py
@@ -0,0 +1,2 @@
+from falco.client import Client  # noqa: F401
+from falco.domain import Request, Response  # noqa: F401
--- falco/utils.py
+++ falco/utils.py
@@ -5,3 +5,8 @@ def pb_timestamp_from_datetime(dt):
     ts = Timestamp()
     ts.FromDatetime(dt)
     return ts
+
+
+def load_file(filepath):
+    with open(filepath, "rb") as f:
+        return f.read()
