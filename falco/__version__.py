7307af85dde6bec57dacdb891575b4428163110f
melmat <mattia@airmail.cc>
2020-02-06 00:31:45 +0100

    Add setup.py and version files
    Signed-off-by: Mattia Meleleo <mattia@airmail.cc>

---
+++ falco/__version__.py
@@ -0,0 +1,8 @@
+__title__ = "falco"
+__description__ = "Python client and SDK for Falco."
+__url__ = "https://github.com/falcosecurity/client-py"
+__version__ = "0.1.0"
+__author__ = "The Falco Authors"
+__author_email__ = ""
+__license__ = "Apache 2.0"
+__copyright__ = "Copyright 2019 The Falco Authors"
