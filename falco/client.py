c8ec67a46913da81521d33f80809abdefa629dd1
Spencer Krum <nibz@spencerkrum.com>
2020-07-07 17:49:40 +0000

    feat(grpc): add unix socket support
    Signed-off-by: Spencer Krum <nibz@spencerkrum.com>

--- falco/client.py
+++ falco/client.py
@@ -8,17 +8,32 @@ from falco.svc.outputs_pb2_grpc import serviceStub
 class InvalidFormat(Exception):
     pass
 
+class TLSConfigError(Exception):
+    print(Exception)
+
 
 class Client:
-    def __init__(self, endpoint, client_crt, client_key, ca_root, output_format=None, *args, **kw):
-        self._client = serviceStub(
-            grpc.secure_channel(
-                endpoint,
-                credentials=get_grpc_channel_credentials(client_crt, client_key, ca_root),
-                options=[("grpc.max_receive_message_length", 1024 * 1024 * 512)],
-            ),
-        )
-        self.output_format = output_format
+    def __init__(self, endpoint, client_crt=None, client_key=None, ca_root=None, output_format=None, *args, **kw):
+        if endpoint.startswith("unix:///"):
+            self._client = serviceStub(
+                grpc.insecure_channel(
+                    endpoint,
+                    options=[("grpc.max_receive_message_length", 1024 * 1024 * 512)],
+                ),
+            )
+            self.output_format = output_format
+
+        else:
+            if None in [client_crt, client_key, ca_root]:
+                raise TLSConfigError("Error: Must provide valid paths to all of the TLS data: client certificate, client key, and CA certificate")
+            self._client = serviceStub(
+                grpc.secure_channel(
+                    endpoint,
+                    credentials=get_grpc_channel_credentials(client_crt, client_key, ca_root),
+                    options=[("grpc.max_receive_message_length", 1024 * 1024 * 512)],
+                ),
+            )
+            self.output_format = output_format
 
     @property
     def output_format(self):
@@ -34,7 +49,7 @@ class Client:
     def subscribe(self, request):  # TODO: test
         pb_req = request.to_proto()
 
-        for pb_resp in self._client.subscribe(pb_req):
+        for pb_resp in self._client.get(pb_req):
             resp = Response.from_proto(pb_resp)
 
             if self.output_format:
--- falco/domain/request.py
+++ falco/domain/request.py
@@ -2,17 +2,15 @@ from falco.schema.outputs_pb2 import request
 
 
 class Request:
-    __slots__ = ("keepalive",)
-
-    def __init__(self, keepalive=None):
-        self.keepalive = keepalive
+    def __init__(self):
+        pass
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(keepalive={self.keepalive})"
+        return f"{self.__class__.__name__}"
 
     @classmethod
-    def from_proto(cls, pb_request):
-        return cls(keepalive=pb_request.keepalive)
+    def from_proto(cls):
+        return cls()
 
     def to_proto(self):
-        return request(keepalive=self.keepalive)
+        return request()
